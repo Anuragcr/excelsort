7f0521c40e055100ba409c68d9b85caeb59e8c84
Anuragcr <65088676+Anuragcr@users.noreply.github.com>
2022-06-25 03:00:28 +0530

    Add files via upload

---
+++ sorterexcel.py
@@ -0,0 +1,26 @@
+# sort an excel file and save it to another file
+
+from tkinter.ttk import Style
+from pandas import Series,DataFrame
+import pandas as pd
+import openpyxl
+pd.set_option('display.max_columns',None)
+
+#source info
+file = input('Enter location/name of file')
+sheet=input('Enter sheet name you wish to sort')
+file2=pd.read_excel(file,sheet,index_col=0,na_values=['NA'])
+
+#Sort Info
+sorter=[]
+style=[]
+sortq=int(input('please enter number of cols to sort:'))
+for x in range(sortq):
+     sorter.append(input(('Name of columns you wish to sort by' )))
+     style.append(input(('Type True for Ascending, False for Descending sort ' )))
+sort1=file2.sort(sorter,ascending=(style))
+
+
+#destination info
+dest=input('enter detination and filename to store as')
+sort1.to_excel(dest,sheet_nmae='Sheet1')
